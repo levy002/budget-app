0f047e0c86dc148a3c7297e002d647b711a83921
levy002 <levy.ukwi002@gmail.com>
2022-07-31 17:11:12 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   
-  resources :categories, only: [:index, :new, :create, :destroy]
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
+    resources :transactions, only: [:new, :create, :destroy]
+  end  
+
   root 'users#index'
 end
