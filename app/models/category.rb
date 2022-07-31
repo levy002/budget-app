9abcc75bbac491a9c9aab49be6f255a6cbc5efff
levy002 <levy.ukwi002@gmail.com>
2022-07-31 17:04:04 +0200

    Add validations and define associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :transactions, dependent: :destroy
 
   validates :name, presence: true, length: { in: 1..50 }
   validates :icon, presence: true
