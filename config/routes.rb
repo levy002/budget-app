f984ada3c4976cd81671b8707c1c75065dbe58ce
levy002 <levy.ukwi002@gmail.com>
2022-08-02 00:25:38 +0200

    Setup routes for splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
-Rails.application.routes.draw do
-  
-  devise_for :users
-  
-  resources :categories, only: [:index, :show, :new, :create, :destroy] do
-    resources :transactions, only: [:new, :create, :destroy]
-  end  
+# frozen_string_literal: true
 
-  root 'users#index'
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users
+
+  resources :categories, only: %i[index show new create destroy] do
+    resources :transactions, only: %i[new create destroy]
+  end
+
+  unauthenticated :user do
+    root 'users#index'
+  end
+
+  authenticated :user do
+    root 'users#show', as: :authenticated_root
+  end
+
 end
