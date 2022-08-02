34aaad953bb139a64e837b7c7ffdbeadd6524c60
levy002 <levy.ukwi002@gmail.com>
2022-08-02 10:27:33 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
+  devise_for :users, controllers: { sessions: 'sessions' }
+  resources :users, only: %i[index]
 
   resources :categories, only: %i[index show new create destroy] do
     resources :transactions, only: %i[new create destroy]
   end
-
-  unauthenticated :user do
-    root 'users#index'
-  end
-
-  authenticated :user do
-    root 'users#show', as: :authenticated_root
-  end
+  
+    resources :splash, only: %i[index]
+    root 'splash#index'
 
 end
